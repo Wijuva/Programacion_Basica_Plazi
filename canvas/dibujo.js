c13186d6ff0a533297d7b63edc89f50fe9aaf907
Wilson Jurado <Wjuradov@correo.udistrital.edu.co>
2020-05-19 19:53:05 -0500

    Clase 14

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,11 +1,24 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
+var lineas = 300;
+var l = 0;
+var aumento = 5;
 
-dibujar_linea("blue", 0, 0, 100, 100)
-dibujar_linea("yellow", 300, 100, 10, 300)
+dibujar_linea("black", 1, 1, 299, 1);
+dibujar_linea("black", 1, 1, 1, 299);
+dibujar_linea("black", 299, 1, 299, 299);
+dibujar_linea("black", 1, 299, 299, 299);
 
-function dibujar_linea(color, xinicial, yincial, xfinal, yfinal)
-{
+while (l < lineas) {
+    dibujar_linea("blue", 0, l, aumento + l, 300);
+    l = l + aumento;
+}
+
+for (let l = 0; l < lineas; l = l + aumento) {
+    dibujar_linea("red", l, 0, 300, aumento + l);
+}
+
+function dibujar_linea(color, xinicial, yincial, xfinal, yfinal) {
     lienzo.beginPath();
     lienzo.strokeStyle = color;
     lienzo.moveTo(xinicial, yincial);
