1f2e3cb1287ab2f81adfe737eb7711eb89cdb2f7
Wilson Jurado <Wjuradov@correo.udistrital.edu.co>
2020-05-19 19:14:10 -0500

    Clase 12

---
+++ canvas/dibujo.js
@@ -0,0 +1,9 @@
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+
+lienzo.beginPath();
+lienzo.strokeStyle = "red";
+lienzo.moveTo(100, 100);
+lienzo.lineTo(200, 200);
+lienzo.stroke();
+lienzo.closePath();
