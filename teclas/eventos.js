3a45de7d5eb19ca097c129921f93f9d2fee4ede4
Wilson Jurado <Wjuradov@correo.udistrital.edu.co>
2020-05-19 23:02:24 -0500

    Clase 17

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -5,28 +5,49 @@ var teclas = {
     RIGHT: 39
 };
 
-document.addEventListener("keyup", dibujarTeclado)
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+var x = 150;
+var y = 150;
+var movimiento = 2;
+var colorcito = "blue";
+
+document.addEventListener("keydown", dibujarTeclado)
 
 function dibujarTeclado(evento) {
     switch (evento.keyCode) {
         case teclas.LEFT:
-            
+            dibujar_linea(colorcito, x, y, x - movimiento, y, lienzo)
+            x = x - movimiento
             break;
 
         case teclas.UP:
-
+            dibujar_linea(colorcito, x, y, x, y - movimiento, lienzo)
+            y = y - movimiento;
             break;
 
         case teclas.RIGHT:
-
+            dibujar_linea(colorcito, x, y, x + movimiento, y, lienzo)
+            x = x + movimiento;
             break;
 
         case teclas.DOWN:
-
+            dibujar_linea(colorcito, x, y, x, y + movimiento, lienzo)
+            y = y + movimiento;
             break;
 
 
         default:
             break;
     }
+}
+
+function dibujar_linea(color, xinicial, yincial, xfinal, yfinal, lienzo) {
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.lineWidth = 2;
+    lienzo.moveTo(xinicial, yincial);
+    lienzo.lineTo(xfinal, yfinal);
+    lienzo.stroke();
+    lienzo.closePath();
 }
