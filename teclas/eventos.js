0b4bd766c242c38ea4fc8533bc468c247860b33d
Wilson Jurado <Wjuradov@correo.udistrital.edu.co>
2020-05-19 22:36:32 -0500

    Clase 16

---
+++ teclas/eventos.js
@@ -0,0 +1,32 @@
+var teclas = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39
+};
+
+document.addEventListener("keyup", dibujarTeclado)
+
+function dibujarTeclado(evento) {
+    switch (evento.keyCode) {
+        case teclas.LEFT:
+            
+            break;
+
+        case teclas.UP:
+
+            break;
+
+        case teclas.RIGHT:
+
+            break;
+
+        case teclas.DOWN:
+
+            break;
+
+
+        default:
+            break;
+    }
+}
